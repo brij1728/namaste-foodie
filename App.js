8669730e05b88852a77554be0c03051edd52a976
Brijesh Kumar <bk10895@gmail.com>
2024-09-18 22:05:35 +0530

    Add styling to the heading element

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
-const heading = React.createElement('h1', null, 'Hello World from React!');
+const heading = React.createElement('h1', {id: 'heading'}, 'Hello World from React!');
+console.log(heading); // object representation of the element
 const root = ReactDOM.createRoot( document.getElementById('root'));
-root.render(heading);
+root.render(heading); // render the element to the DOM
