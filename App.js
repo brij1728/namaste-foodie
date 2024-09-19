224d7292cffd289eed87eef6975d5516fcfe7e1c
Brijesh Kumar <bk10895@gmail.com>
2024-09-19 21:37:01 +0530

    Refactor App.js to render a parent div with child divs and a heading element using functional components

--- App.js
+++ App.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // React Element
+
+
+const elem = <span>React Element</span>;
+
+const title = (
+  <h1 id="title" className="title">
+    {elem}
+    Namaste React
+    
+  </h1>
+)
+
+// React functional component
 const Title = () => (
   <h1 id="title" className="title">
     Namaste React using JSX
@@ -12,7 +25,10 @@ const number = 10;
 
 const HeadingComponent = () => (
   <div id="container">
+    {Title()}
     <Title />
+    <Title></Title>
+    {title}
     <h2>{number}</h2>
     <h1 id="heading" className="head">
       Namaste React using Functional Component
