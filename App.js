4fd02120f2af4561860d0d66badd66c29968de39
Brijesh Kumar <bk10895@gmail.com>
2024-09-19 11:07:31 +0530

    Refactor App.js to render a parent div with child divs and a heading element

--- App.js
+++ App.js
@@ -1,14 +1,4 @@
-/*
 
-<div id='parent'>
-	<div id='child>
-		<h1 id="heading">Hello World from React!</h1>
-	</div>
-	<div id='child>
-		<h1 id="heading">Greeting from Mars!</h1>
-	</div>
-</div>
- */
 
 const parent = React.createElement('div', {id: 'parent'},
 	React.createElement('div', {id: 'child'},
@@ -19,8 +9,7 @@ const parent = React.createElement('div', {id: 'parent'},
 	),
 	
 );
-// const heading = React.createElement('h1', {id: 'heading'}, 'Hello World from React!');
-// console.log(heading); // object representation of the element
+
 const root = ReactDOM.createRoot( document.getElementById('root'));
-// root.render(heading); // render the element to the DOM
 root.render(parent);
+
