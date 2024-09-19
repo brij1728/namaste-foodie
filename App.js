e9f6a82ce3d4c1d150f3eb432c4ae6cb84c19abf
Brijesh Kumar <bk10895@gmail.com>
2024-09-19 17:26:24 +0530

    Refactor .gitignore and package.json, and update App.js and index.html

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const parent = React.createElement('div', {id: 'parent'},
 	React.createElement('div', {id: 'child'},
-		[React.createElement('h1', {}, 'Hello World from React World!'), React.createElement('p', {}, 'This is a paragraph')]
+		[React.createElement('h1', {}, 'Hello World from React World!'), React.createElement('p', {}, 'This is a Brijesh Singh')]
 	),
 	React.createElement('div', {id: 'child'},
-		[React.createElement('h1', {}, 'HGreeting from Mars!'), React.createElement('p', {}, 'This is a paragraph')]
+		[React.createElement('h1', {}, 'HGreeting from Mars!'), React.createElement('p', {}, 'This is a Ballia')]
 	),
 	
 );
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # namaste-react
+
+
+
+# Parcel
+- Dev Build
+- Local Build
+- HMR (Hot Module Replacement)
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification in Production
+- Bundling
+- Compress
+- Consistent Hashing
+- Code SPlitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- Hosting App on https
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
