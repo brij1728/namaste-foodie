98e427af1a93aff1224848031b197ce3b9c2de88
Brijesh Kumar <bk10895@gmail.com>
2024-10-05 08:07:57 +0530

    Refactor Contact component tests for Contact Us page

--- src/components/Contact/Contact.test.js
+++ src/components/Contact/Contact.test.js
@@ -1,15 +1,48 @@
 import '@testing-library/jest-dom';
 
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react';
 
-import { Contact } from "./Contact";
+import { Contact } from './Contact';
 
-test("Should load contact us component", () => {
-	
-	render(<Contact />); //Render the Contact component to jsdom
+describe(' Contact Us Page Teast Cases', () => {
+	// can use it or test
+  it('Should load contact us component', () => {
+    render(<Contact />); //Render the Contact component to jsdom
 
-	const heading = screen.getByRole("heading", {name: "Contact"}); //Assert that the Contact Us heading is present
-	const button = screen.getByRole("button", {name: "Submit"}); //Assert that the Submit button is present
+    const heading = screen.getByRole('heading', { name: 'Contact' }); //Assert that the Contact Us heading is present
+    //const button = screen.getByRole("button", {name: "Submit"}); //Assert that the Submit button is present
 
-	expect(button).toBeInTheDocument(); //Assert that the Contact Us heading is present
-})
+    expect(heading).toBeInTheDocument(); //Assert that the Contact Us heading is present
+  });
+
+  test('Should load button inside Contact component', () => {
+    render(<Contact />); //Render the Contact component to jsdom
+
+    const button = screen.getByRole('button', { name: 'Submit' }); //Assert that the Submit button is present
+
+    //Assert that the Contact Us heading is present
+    expect(button).toBeInTheDocument();
+  });
+
+  test('Should load input fields inside Contact component', () => {
+    render(<Contact />); //Render the Contact component to jsdom
+
+    const inputName = screen.getByPlaceholderText('Name'); //Assert that the input fields are present
+
+    //Assert that the input fields are present
+    expect(inputName).toBeInTheDocument();
+  });
+
+  test('Should load 2 input fields inside Contact component', () => {
+    //Render the Contact component to jsdom
+    render(<Contact />);
+
+    //Assert that the input fields are present
+    const inputFields = screen.getAllByRole('textbox');
+
+    console.log(inputFields.length);
+
+    //Assert that the input fields are present
+    expect(inputFields.length).toBe(3);
+  });
+});
